9b4d56b27d7bc391976d08b93e240f69a444e084
seltikc@yandex.ru <info@seo-effect.ru>
2021-03-07 23:16:37 +0500

    Модальное окно на главной

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const modal = document.querySelector(".feeldback");
+const btnOpen = document.querySelector(".delivery__btn");
+const btnClose = document.querySelector(".modal-close");
+
+
+btnOpen.addEventListener("click", function (evt) { 
+    evt.preventDefault();
+    modal.classList.add("feeldback-show");
+});
+
+btnClose.addEventListener("click", function (evt) { 
+    evt.preventDefault();
+    modal.classList.remove("feeldback-show");
+});
